f89b6f07e60c6937361ee2b520082f9f8bfc898a
Remedeae <sanna.m.blomdahl@gmail.com>
2025-01-07 07:09:49 +0100

    naviation, header and presentation

---
+++ Protfolio-remedeae/portfolio-app/src/navbar.jsx
@@ -0,0 +1,17 @@
+
+
+function Navbar() {
+
+    return (
+        <nav>
+            <div></div>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
